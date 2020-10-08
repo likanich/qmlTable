787b21a95f576cb0fc3bbb22d1a07b2b597f60fc
Vasiliy Kamagorov <likanich152@gmail.com>
2020-10-08 13:55:58 +0700

    First commit. Created TableView with headers

---
+++ qmlTable/main.cpp
@@ -0,0 +1,18 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
+#include "tablemodel.h"
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+    QQmlApplicationEngine engine;
+    TableModel model;
+
+    engine.rootContext()->setContextProperty("table_model", &model);
+
+    const QUrl url(QStringLiteral("qrc:/main.qml"));
+    engine.load(url);
+
+    return app.exec();
+}
